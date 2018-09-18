75ee5cef437feabcb56242b8a2a0929f1281147a
dmitrygorsky <43175416+dmitrygorsky@users.noreply.github.com>
2018-09-18 13:23:07 +0300

    Initial commit

---
+++ by/cdptr/javabasics/l1_5/SumFourDigits.java
@@ -0,0 +1,13 @@
+package by.cdptr.javabasics.l1_5;
+
+public class SumFourDigits {
+
+    public static double sum (double a, double b, double c, double d) {
+
+        double result = a + b + c + d;
+
+        return result;
+
+    }
+
+}
