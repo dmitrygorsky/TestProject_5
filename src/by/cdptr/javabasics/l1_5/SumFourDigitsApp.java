d8e4b69c8c44d06e81434f97775c73a40fbff041
dmitrygorsky <43175416+dmitrygorsky@users.noreply.github.com>
2018-09-19 21:06:19 +0300

    Add files via upload

---
+++ src/by/cdptr/javabasics/l1_5/SumFourDigitsApp.java
@@ -0,0 +1,17 @@
+package by.cdptr.javabasics.l1_5;
+
+public class SumFourDigitsApp {
+
+    public static void main(String[] args) {
+
+        double a = 12.52;
+        double b = 46.64;
+        double c = 7.01;
+        double d = 99.90;
+
+        double result = a + b + c + d;
+
+        System.out.println("Сумма равняется " + result);
+
+    }
+}
